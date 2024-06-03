c5a48fe3faa3b9f1f800ca7b4b31b7d43da91cc7
KettelBear <kettel.bear@gmail.com>
2024-06-02 21:45:18 -0400

    The Next Section Begins: Leagues

--- lib/ex_clash/clan/capital.ex
+++ lib/ex_clash/clan/capital.ex
@@ -1,5 +1,7 @@
 defmodule ExClash.Clan.Capital do
   @moduledoc """
+  /capitalleagues
+  /capitalleagues/{leagueId}
   /clans/{clanTag}/capitalraidseasons
   """
   alias ExClash.Clan.Capital.District
@@ -11,6 +13,17 @@ defmodule ExClash.Clan.Capital do
 
   defstruct [:capital_hall_level, :districts]
 
+  def leagues(params \\ []) do
+    case ExClash.HTTP.get("/leagues", params) do
+      {:ok, body} -> body
+      err -> err
+    end
+  end
+
+  @doc """
+  Format Supercell's for the Clan Capital.
+  """
+  @spec format(api_capital :: ExClash.cell_map()) :: __MODULE__.t()
   def format(nil), do: nil
   def format(api_capital) do
     districts = Map.get(api_capital, "districts", [])
