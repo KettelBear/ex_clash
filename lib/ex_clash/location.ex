d25c6744306a87cfe011023446474537b96a83e5
KettelBear <kettel.bear@gmail.com>
2024-06-24 17:32:50 -0400

    Add Locations List Call
    Adding the ability to get the location information with pagination
    options available.

--- lib/ex_clash/location.ex
+++ lib/ex_clash/location.ex
@@ -3,13 +3,9 @@ defmodule ExClash.Location do
   The Location struct.
 
   Attributes:
-
     * `id` - The location identification number.
-
     * `name` - The name of the location.
-
     * `is_country` - Boolean stating if the location is a country.
-
     * `country_code` - The country code.
   """
 
@@ -21,4 +17,15 @@ defmodule ExClash.Location do
   }
 
   defstruct [:id, :name, :is_country, :country_code]
+
+  @spec list(opts :: Keyword.t()) :: {list(__MODULE__.t()), ExClash.Paging.t()} | {:error, atom()}
+  def list(opts \\ []) do
+    case ExClash.HTTP.get("/locations", opts) do
+      {:ok, %{"items" => items, "paging" => pagination}} ->
+        {ExClash.HTTP.resp_to_struct(items, __MODULE__), ExClash.Paging.format(pagination)}
+
+      err ->
+        err
+    end
+  end
 end
